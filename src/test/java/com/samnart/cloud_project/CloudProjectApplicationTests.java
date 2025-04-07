540509079175e7e17df849d1592d1cb63419985f
samnart1 <originalsamnart@gmail.com>
2025-04-07 16:54:51 +0200

    initial commit

---
+++ src/test/java/com/samnart/cloud_project/CloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samnart.cloud_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
